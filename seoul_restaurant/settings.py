61c9c5dfd584938dcd2cb29b0d800b9610656ef5
Fabian <fabianjung228@gmail.com>
2024-07-11 22:12:12 -0300

    updated

--- seoul_restaurant/settings.py
+++ seoul_restaurant/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/website/static/'
-
-
+STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+STATIC_ROOT ='/Users/Fabian/Desktop/vscode/fixed_seoul_restaurant/website/static'
 
 
 
