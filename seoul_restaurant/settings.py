7baf6c4719fb9b8e81664d2607df1f04d9df4608
Fabian <fabianjung228@gmail.com>
2024-07-11 22:21:17 -0300

    updated

--- seoul_restaurant/settings.py
+++ seoul_restaurant/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n+i#@x4pm$^oxf&9ra09&+*letqh+r@&x%-+^djopjc_(5!af5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
@@ -123,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
-STATIC_ROOT ='/Users/Fabian/Desktop/vscode/fixed_seoul_restaurant/website/static'
+STATIC_URL = '/static'
+STATIC_ROOT = (os.path.join(BASE_DIR,'staticfiles'),)
+#STATIC_ROOT ='/Users/Fabian/Desktop/vscode/fixed_seoul_restaurant/website/static'
 
 
 
