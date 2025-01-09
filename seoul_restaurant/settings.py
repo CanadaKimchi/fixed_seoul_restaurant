75370e3c6a254b894b3f613d3430b05eee925650
Euisung <fabianjung228@gmail.com>
2025-01-08 21:03:29 -0400

    Update settings.py

--- seoul_restaurant/settings.py
+++ seoul_restaurant/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n+i#@x4pm$^oxf&9ra09&+*letqh+r@&x%-+^djopjc_(5!af5'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
